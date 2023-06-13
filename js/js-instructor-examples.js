2d8ea9a08f5a8222473c95de8db05618071c1786
Ihor Kaniuk <ikanyuk@icloud.com>
2023-06-13 19:14:31 +0300

    start

---
+++ js/js-instructor-examples.js
@@ -0,0 +1,29 @@
+// ---------------------- REPETA---------------------------------
+
+// js/03-numbers.js
+
+/*
+ * Напиши скрипт который просит пользователя ввести число и степень,
+ * возводит число в эту степень и выводит результат в консоль
+ */
+
+// const userNumber = Number(prompt('Enter Number'));
+// const power = Number(prompt('Enter power'));
+
+// console.log(userNumber);
+// console.log(power);
+// console.log(Math.pow(userNumber, power));
+
+// ----------------------------------------------------------
+// js/04-strings.js
+
+/*
+ * Напиши скрипт который выведет строку в формате:
+ * «Гость x y поселяется в z номер q»,
+ * подставив вместо x y z q значения переменных
+ */
+
+// const firstName = '';
+// const lastName = '';
+// const type = '';
+// const room = 1;
