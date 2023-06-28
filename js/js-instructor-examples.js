76f60d478244cb0470195ab712a760b075d7b3d7
Ihor Kaniuk <ikanyuk@icloud.com>
2023-06-28 21:42:23 +0300

    add-inctructor-file

---
+++ js/js-instructor-examples.js
@@ -0,0 +1,27 @@
+// Модуль 1. Заняття 1. Змінні, типи та оператори
+
+// Example 1 - Математичні оператори
+// Виведи на екран загальну кількість яблук та винограду. Різницю яблук та винограду.
+
+// const apples = 47;
+// const grapes = 135;
+// const total = apples + grapes;
+// console.log(total);
+// const diff = grapes - apples;
+// console.log(diff);
+
+
+// Example 2 - Комбіновані оператори
+// 3міни вираз перевизначення комбінованим оператором +=.
+
+// let students = 100;
+// students += 50;
+// console.log(students);
+
+
+// Example 3 - Пріоритет операторів
+
+// Розбери пріоритет операторів в інструкції привласнення значення змінної result.
+
+// const result = 108 + 223 - 2 * 5;
+// console.log(result);
