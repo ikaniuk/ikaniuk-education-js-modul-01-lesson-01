e0948dff14bb91a879997ee636231dcc64200a24
Ihor Kaniuk <ikanyuk@icloud.com>
2023-07-10 18:27:49 +0300

    ыв

--- js/js-instructor-examples.js
+++ js/js-instructor-examples.js
@@ -25,3 +25,41 @@
 
 // const result = 108 + 223 - 2 * 5;
 // console.log(result);
+
+
+// Example 4 - Клас Math
+
+// Напиши скрипт, який виводить у консоль заокруглені вгору / вниз і т.д.значення змінної value.
+// Використовуй методи Math.floor(), Math.ceil() та Math.round().Перевір що буде в консолі при
+// значеннях 27.3 та 27.9.
+
+// const value = 27.9;
+
+// console.log(Math.floor(value));
+// console.log(Math.ceil(value));
+// console.log(Math.round(value))
+
+
+// Example 6 - Методи рядків та чейнінг
+
+// Напиши скрипт, який розраховує індекс маси тіла людини.Для цього необхідно розділити вагу
+// в кілограмах на квадрат висоти людини у метрах.
+
+// Вага та висота зберігаються у змінних weight та height, але не як числа, а в вигляді рядків
+// (спеціально для завдання).Не цілі числа можуть бути задані у вигляді 24.7 або 24, 7, тобто
+// як роздільник дробової частини може бути кома.
+
+// Індекс маси тіла необхідно округлити до однієї цифри після коми;
+
+// let weight = '88,3';
+// // weight = weight.replaceAll(',', '.');
+// // weight = Number.parseFloat(weight);
+// weight = Number.parseFloat(weight.replaceAll(',', '.'));
+// let height = '1.75';
+// // height = height.replaceAll(',', '.');
+// // height = Number.parseFloat(height);
+// height = Number.parseFloat(height.replaceAll(',', '.'));
+
+
+// const bmi = weight / Math.pow(height, 2);
+// console.log(Number(bmi.toFixed(1))); // 28.8
