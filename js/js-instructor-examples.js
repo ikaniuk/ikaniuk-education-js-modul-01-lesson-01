118a292392e8a2af5721b84881a417ed34870307
Ihor Kaniuk <ikanyuk@icloud.com>
2023-07-10 19:00:03 +0300

    complete

--- js/js-instructor-examples.js
+++ js/js-instructor-examples.js
@@ -37,8 +37,18 @@
 
 // console.log(Math.floor(value));
 // console.log(Math.ceil(value));
-// console.log(Math.round(value))
+// console.log(Math.round(value));
 
+// Example 5 - Шаблонные строки
+
+// Составь фразу с помощью шаблонных строк A has B bots in stock,
+// где A, B - переменные вставленные в строку.
+
+// const companyName = 'Cyberdyne Systems';
+// const repairBots = 150;
+// const defenceBots = 50;
+// const message = `${companyName} has ${repairBots + defenceBots} bots in stock`;
+// console.log(message); // "Cyberdyne Systems has 200 bots in stock"
 
 // Example 6 - Методи рядків та чейнінг
 
@@ -63,3 +73,36 @@
 
 // const bmi = weight / Math.pow(height, 2);
 // console.log(Number(bmi.toFixed(1))); // 28.8
+
+
+// Example 9 - Значение по умолчанию и оператор нулевого слияния
+// Отрефактори код так, чтобы в переменную value присваивалось значение
+// переменной incomingValue, если оно не равно undefined или null.В противном
+// случае должно присваиваться значение defaultValue.Проверь работу скрипта для
+// слепдующих значений переменной incomingValue: null, undefined, 0, false.
+// Используй оператор ?? (nullish coalescing operator).
+
+// const incomingValue = false;
+// const defaultValue = 10;
+// // const value = incomingValue || defaultValue;
+// const value = incomingValue === null || incomingValue === undefined ? defaultValue : incomingValue;
+// console.log(value);
+
+
+// Example 10 - Опертор % и методы строк
+// Напиши скрипт который переведёт значение totalMinutes(количество минут) в строку в 
+// формате часов и минут HH: MM.
+
+// 70 покажет 01:10
+// 450 покажет 07:30
+// 1441 покажет 24:01
+// const totalMinutes = 70;
+
+// const hours = Math.floor(totalMinutes / 60);
+// const minutes = totalMinutes % 60;
+// console.log(hours);
+// console.log(minutes);
+
+// const doubleDigitHours = String(hours).padStart(2, 0);
+// const doubleDigitMinutes = String(minutes).padStart(2, 0);
+// console.log(`${doubleDigitHours}:${doubleDigitMinutes}`);
